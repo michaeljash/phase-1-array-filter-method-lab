51fdc859faefef01e056ade4a30196ac843ea14b
jashonmichael <jashon.michael@student.moringaschool.com>
2024-03-26 13:50:04 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+// Function to find matching drivers
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+  
+  // Function to fuzzy match drivers
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+  
+  // Function to match driver names
+  function matchName(driverObjects, query) {
+    return driverObjects.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+  }
+  
